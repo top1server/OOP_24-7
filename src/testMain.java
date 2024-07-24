bb16350d8a0ab0bfd57506d9683f4b93f4a43a42
top1server <161910784+top1server@users.noreply.github.com>
2024-07-24 12:33:40 +0700

    Update testMain.java

--- src/testMain.java
+++ src/testMain.java
@@ -14,8 +14,8 @@ public class testMain {
         manage.addTeam(t);
         manage.addTeam(t1);
 
-        plantRobot farmbot = new plantRobot("Duc", 2, "123", "cay tung");
-        Robot fishbot = new fishRobot("Dung", 5, "456", "give fish eating");
+        PlantRobot farmbot = new PlantRobot("Duc", 2, "123", "cay tung");
+        Robot fishbot = new FishRobot("Dung", 5, "456", "give fish eating");
         farmbot.robotHarvest();
         manage.addRobotThroughTeam(t, farmbot);
         manage.addRobotThroughTeam(t, fishbot);
